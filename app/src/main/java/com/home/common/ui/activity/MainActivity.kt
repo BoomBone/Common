6a41d23323fd528ccdb0a3765d0b89e516ff5de0
BoomBone <ticccccc@163.com>
2018-03-19 21:25:43 +0800

    app添加base模块依赖

--- app/src/main/java/com/home/common/ui/activity/MainActivity.kt
+++ app/src/main/java/com/home/common/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.home.common
+package com.home.common.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.home.common.R
 
 class MainActivity : AppCompatActivity() {
 
